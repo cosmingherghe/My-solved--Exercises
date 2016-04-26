6043d6de2ce021c6ee4ccda537de21e4ae6239b6
Cosmin Gherghe <cosminlinux@gmail.com>
2016-04-26 15:43:52 +0100

    Added files via upload

---
+++ Chapter1_1.java
@@ -0,0 +1,19 @@
+
+public class Chapter1_1 {
+
+	/**
+	 * EXERCISE 1
+	 * (Display three messages) Write a program that displays Welcome to Java , 
+	 * 	Welcome to Computer Science , and Programming is fun .
+	 */
+	
+	public static void main(String[] args) {
+		
+		String newLine = System.getProperty("line.separator");	//This will retrieve line separator dependent on OS.
+		
+		System.out.println( "Welcome to Java" + newLine + 
+							"Welcome to Computer Science" + newLine + 
+							"Programming is fun" );
+	}
+
+}
