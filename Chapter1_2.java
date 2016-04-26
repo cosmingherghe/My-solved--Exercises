b5f5bd153e1ff9f2b78126a9f8c73bd605899850
Cosmin Gherghe <cosminlinux@gmail.com>
2016-04-26 16:05:37 +0100

    Added files via upload

---
+++ Chapter1_2.java
@@ -0,0 +1,15 @@
+
+public class Chapter1_2 {
+
+	/**
+	 * Exercise 2 (Display five messages)
+	 */
+	
+	public static void main(String[] args) {
+		
+		for( int i=0; i<4; i++) {
+		
+			System.out.println("Welcome to Java");
+		}
+	}
+}
