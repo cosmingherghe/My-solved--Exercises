580da436acf776e11bb2d459000b6fea161070e3
Cosmin Gherghe <cosminlinux@gmail.com>
2016-04-26 17:12:28 +0100

    Added files via upload

---
+++ Chapter1_5.java
@@ -0,0 +1,14 @@
+
+public class Chapter1_5 {
+
+	/**
+	 * Exercice 5 (Compute expresion - Result)
+	 * 
+	 * (9.5 x 4.5 - 2.5 x 3) / (45.5 - 3.5)
+	 */
+	public static void main(String[] args) {
+	
+		System.out.print((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+		
+	}
+}
