d7b895c8e7c59ffefba5c4a68b74bcf268735683
Cosmin Gherghe <cosminlinux@gmail.com>
2016-04-29 19:14:24 +0100

    Added files via upload

---
+++ Chapter1_7.java
@@ -0,0 +1,31 @@
+public class Chapter1_7 {
+
+	/**
+	 *  Exercise 7 
+	 *  display result:
+	 *  	- 4 x ( 1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 )
+	 *  	- 4 x (  1 - 1/3 + 1/5 - 1/7 + 1/9 - 1/11 + 1/13 )
+	 */
+
+	public static void main(String[] args) {
+		
+		int oddno = 6;	// initialise how many odd numbers we need
+		int startodd = 1;	//initialise from where odd numbers start		
+		double inbrace = 1.0;	//initialise the operations in braces
+		
+		for( int i = 2; i<oddno+2; i++){
+
+			startodd += 2; //getting the next odd number 
+			
+			//staring with the first number after 1 for each number is adding - and +, and starts calculating
+			if( (i%2 == 0)){
+				inbrace -= 1.0/i;
+			}
+			else { 
+				inbrace += 1.0/i;
+			}
+		}
+		
+		System.out.println(4*inbrace); // the result in braces is multiplied by 4
+	}
+}
