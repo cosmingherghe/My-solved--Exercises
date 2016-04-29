c36d29777f4a7a54a4768b4894168dc08b8fbe77
Cosmin Gherghe <cosminlinux@gmail.com>
2016-04-29 20:20:45 +0100

    Added files via upload

---
+++ Chapter1_8.java
@@ -0,0 +1,17 @@
+
+public class Chapter1_8 { 
+
+	/**
+	 * 	Exercise 8 
+	 * 	display area and perimeter of a circle that has a radius of 5.5
+	 */
+	public static void main(String[] args) {
+		
+		double radius = 5.5;
+		
+		System.out.println( "Perimeter = " + 2 * radius * Math.PI);
+		
+		System.out.println( "Area = " + radius * radius * Math.PI);
+	}
+
+}
