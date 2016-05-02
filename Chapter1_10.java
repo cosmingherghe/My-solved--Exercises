1c93711a326b3f43e0fc2015669516b345912648
Cosmin Gherghe <cosminlinux@gmail.com>
2016-05-02 19:12:40 +0100

    Create Chapter1_10.java

---
+++ Chapter1_10.java
@@ -0,0 +1,19 @@
+public class Chapter1_10 {
+	
+	/**
+	 *   Exercise 10 
+	 * 	 Displays the average speed in miles per hour
+	 *   Assume a runner runs 18 kilometers in 1hour, 35 minutes and 40 seconds
+	 */
+	 
+	public static void main (String[] args) {
+
+		double kmran = 18.0;
+		double tran = ((60 + 35) * 60) + 40.0;  // we transform the entire time in seconds 
+		
+		double mps = ((18.0/1.6)/tran);  // we calculate the average mile per second speed
+		
+		
+		System.out.println("The average speed is " + (double)Math.round(mps * 3600*100)/100 + " miles per hour: " );	// we display the average speed in miles per hour (round up to 2 decimal places)
+	}
+}
