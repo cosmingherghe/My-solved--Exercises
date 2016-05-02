f57fd527aadb5be23fec748f77800ceac4253aee
Cosmin Gherghe <cosminlinux@gmail.com>
2016-05-02 15:18:58 +0100

    Create Chapter1_9.java

---
+++ Chapter1_9.java
@@ -0,0 +1,17 @@
+public class Chapter1_9 {
+ 
+	/**
+	 * 	Exercise 9 
+	 * 	 displays the area and perimeter of a rectangle
+	 */
+ 
+	public static void main (String[] args) {
+ 
+		double width = 4.5;
+		double height = 7.9;
+ 
+		System.out.println( "Perimeter = " + width + height);
+ 
+		System.out.println( "Area = " + width * height);
+	}
+}
