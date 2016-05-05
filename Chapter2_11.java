be69fc39f3529174121d72f9142f2ea7333fa8ad
Cosmin Gherghe <cosminlinux@gmail.com>
2016-05-05 14:44:37 +0100

    Added files via upload

---
+++ Chapter2_11.java
@@ -0,0 +1,17 @@
+
+public class Chapter2_11 {
+
+	/*
+	 * Display results for reminders 
+	 */
+	public static void main(String[] args) {
+
+		System.out.println("67 % 7 = " + 67 % 7
+				+ "\n87 % -5 = " + 87 % -5
+				+ "\n-45 % 6 = " + -45 % 6
+				+ "\n-45 % -6 = " + -45 % -6
+				+ "\n6 % 2 = " + 6 % 2
+				+ "\n2 % 6 = " + 2 % 6	);
+	}
+
+}
