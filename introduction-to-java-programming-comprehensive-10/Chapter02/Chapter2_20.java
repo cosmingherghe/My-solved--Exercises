26b8d2da8ced08c5a6e33e292db957f086d91cb2
Cosmin Gherghe <cosminlinux@gmail.com>
2016-05-09 23:49:28 +0100

    Add files via upload

---
+++ introduction-to-java-programming-comprehensive-10/Chapter02/Chapter2_20.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+
+public class Chapter2_20 {
+	/**
+	 *		Financial application: calculate interest
+	 *
+	 *		interest = balance * (annualInterestRate/1200)
+	 */
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println( "Enter balance and interest rate (e.g., 3 for 3%): ");
+
+		double balance = input.nextDouble();
+		double annualInterestRate = input.nextDouble();
+		
+		double interest = balance * (annualInterestRate/1200);
+
+		System.out.println( "The interest is: " + interest );
+
+	}
+}
