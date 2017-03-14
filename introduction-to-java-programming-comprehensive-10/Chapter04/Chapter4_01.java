60f993633b16bd2b108bb7d26c6f34315772c568
Cosmin Gherghe <cosminlinux@gmail.com>
2017-03-14 13:10:14 +0000

    Create Chapter4_01.java

---
+++ introduction-to-java-programming-comprehensive-10/Chapter04/Chapter4_01.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+	
+  System.out.println("(a) Math.sqrt(4) is " + Math.sqrt(4)
+	        + "\n(b) Math.sin(2 * Math.PI) is " + Math.sin(2 * Math.PI)
+	        + "\n(c) Math.cos(2 * Math.PI) is " + Math.cos(2 * Math.PI)
+		        + "\n(d) Math.pow(2, 2) is " + Math.pow(2, 2)
+		        + "\n(e) Math.log(Math.E) is " + Math.log(Math.E)
+		        + "\n(f) Math.exp(1) is " + Math.exp(1)
+		        + "\n(g) Math.max(2, Math.min(3, 4)) is " + Math.max(2, Math.min(3, 4))
+		        + "\n(h) Math.rint(-2.5) is " + Math.rint(-2.5)
+		        + "\n(i) Math.ceil(-2.5) is " + Math.ceil(-2.5)
+		        + "\n(j) Math.floor(-2.5) is " + Math.floor(-2.5)
+		        + "\n(k) Math.round(-2.5f) is " + Math.round(-2.5f)
+		        + "\n(l) Math.round(-2.5) is " + Math.round(-2.5)
+		        + "\n(m) Math.rint(2.5) is " + Math.rint(2.5)
+		        + "\n(n) Math.ceil(2.5) is " + Math.ceil(2.5)
+		        + "\n(o) Math.floor(2.5) is " + Math.floor(2.5)
+		        + "\n(p) Math.round(2.5f) is " + Math.round(2.5f)
+		        + "\n(q) Math.round(2.5) is " + Math.round(2.5)
+		        + "\n(r) Math.round(Math.abs(-2.5)) is " + Math.round(Math.abs(-2.5)) );
+	}
+}
+
+
+
