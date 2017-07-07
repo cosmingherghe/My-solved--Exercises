c1ecaafe7ebfd00b0ba5050244d610ab363b085a
Cosmin Gherghe <cosminlinux@gmail.com>
2017-07-07 13:25:21 +0100

    Create Chapter4_16.java

---
+++ introduction-to-java-programming-comprehensive-10/Chapter04/Chapter4_16.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+    	//The returned value is no lower than (and may possibly equal) min, and is less than (but not equal to) max.
+	    System.out.print( (char)(Math.random() * (90 - 65) + 65) );
+  
+    }
+}
