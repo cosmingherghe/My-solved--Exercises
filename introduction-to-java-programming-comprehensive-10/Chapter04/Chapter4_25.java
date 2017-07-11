444ad4e750b36b562e708679adc522fa13af8a68
Cosmin Gherghe <cosminlinux@gmail.com>
2017-07-11 18:54:02 +0100

    Create Chapter4_25.java

---
+++ introduction-to-java-programming-comprehensive-10/Chapter04/Chapter4_25.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class HelloWorld {
+  public static void main(String[] args) {
+  
+  	//(int)(Math.random() * ((upperbound - lowerbound) + 1) + lowerbound); 
+    	//(where lowerbound is inclusive and upperbound exclusive).
+ 	int  plateNumber = (int)( Math.random() * ((10000 - 1000) + 1) + 1000 );
+ 	
+ 	//Character.toString(char)  https://stackoverflow.com/questions/8172420/how-to-convert-a-char-to-a-string
+ 	String plateThreeLetters = 	Character.toString( (char)(int)( Math.random() * ((91 - 65) + 1) + 65 ) ) +
+ 					Character.toString( (char)(int)( Math.random() * ((91 - 65) + 1) + 65 ) ) +
+ 					Character.toString( (char)(int)( Math.random() * ((91 - 65) + 1) + 65 ) );
+	
+	System.out.print( "Generated vehicle plate number is: " + plateThreeLetters + plateNumber );	
+  
+  }
+}
