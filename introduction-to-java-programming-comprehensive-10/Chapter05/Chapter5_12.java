5ce717a0b28892f0aa9c2b318faf23920e90ea46
Cosmin Gherghe <cosminlinux@gmail.com>
2017-07-26 13:22:18 +0100

    Create Chapter5_12.java

---
+++ introduction-to-java-programming-comprehensive-10/Chapter05/Chapter5_12.java
@@ -0,0 +1,15 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+	 
+		int i = 1;
+	
+		System.out.print( "A while loop finds that ");
+	 
+		while(Math.pow(i,2) < 12000) { 
+			i++; 
+	  	}
+	  	
+	  	System.out.print( i + " is the smallest integer n such that n^2 is greater than 12,000.");
+
+	}
+}
