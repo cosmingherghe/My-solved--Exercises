4829b49806077f90cf96dc6dab0113a3f9a4c791
Cosmin Gherghe <cosminlinux@gmail.com>
2017-08-17 14:13:07 +0100

    Create Chapter5_35.java

---
+++ introduction-to-java-programming-comprehensive-10/Chapter05/Chapter5_35.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+	public static void main(String[] args) {
+ 
+		double sum = 0;
+		
+		for(int i=1; i<625; i++){ 
+			sum += 1/(Math.sqrt(i) + Math.sqrt(i+1));
+			//TEST System.out.println( sum + " = 1/" + "Math.sqrt(" + i + ") + Math.sqrt("+ (i+1) + "));");
+		} 
+			System.out.println(sum);
+			
+	}
+}
