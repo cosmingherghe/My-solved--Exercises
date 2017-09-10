49545054eec580e9cc17cbdeb2dbb7bd75933836
Cosmin Gherghe <cosminlinux@gmail.com>
2017-09-10 18:57:28 +0100

    Update Chapter6_19.java

--- introduction-to-java-programming-comprehensive-10/Chapter06/Chapter6_19.java
+++ introduction-to-java-programming-comprehensive-10/Chapter06/Chapter6_19.java
@@ -8,7 +8,34 @@
 *	public static double area(double side1, double side2, double side3)
 *	
 *	Write a test program that reads three sides for a triangle and computes the area if
-*	the input is valid. Otherwise, it displays that the input is invalid. The formula for
-*	computing the area of a triangle is given in Programming Exercise 2.19.
+*	the input is valid. Otherwise, it displays that the input is invalid. 
 */
 
+public class MyTriangle {
+	public static void main(String[] args) {
+
+		double side1 = 2;
+		double side2 = 3;
+		double side3 = 4;
+		
+		if( isValid(side1, side2, side3) ) {
+			System.out.print("The input is invalid.");
+		}
+		else {
+			System.out.printf("Area: %f", area(side1, side2, side3) );
+		}
+	}
+	
+	/** Return the area of the triangle. */
+	public static double area(double side1, double side2, double side3){
+		double hp = (side1 + side2 + side3) / 2; // compute half the perimeter
+		return Math.sqrt(hp * (hp-side1) * (hp-side2) * (hp-side3)); //compute area
+	}
+	
+	/** Return true if the sum of any two sides is greater than the third side. */
+	public static boolean isValid(double side1, double side2, double side3) {
+		return (side1 > side2 + side3) || (side2 > side1 + side3) || (side3 > side1 + side2);
+	}
+	
+	
+}
