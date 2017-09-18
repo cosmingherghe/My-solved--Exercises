e3e36df25208f58828337ee4de7c4a1528873c50
Cosmin Gherghe <cosminlinux@gmail.com>
2017-09-18 09:28:40 +0100

    Create Chapter6_35.java

---
+++ introduction-to-java-programming-comprehensive-10/Chapter06/Chapter6_35.java
@@ -0,0 +1,26 @@
+/**
+*       (Geometry: area of a pentagon) The area of a pentagon can be computed using a formula
+*       Write a method that returns the area of a pentagon using the following header:
+*       public static double area(double side)
+*       Write a main method that prompts the user to enter the side of a pentagon and
+*       displays its area.
+*/
+
+import java.util.*;
+
+public class pentagonArea {
+
+	public static void main(String[] args) {
+
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the side:");
+		
+		double side = input.nextDouble();
+		
+		System.out.printf("The area of the pentagon is %f", area(side));
+	}
+	
+	public static double area(double side) {
+		return ( 5 * Math.pow(side, 2) ) / ( 4 * Math.tan(Math.PI/5) );
+	}
+}
