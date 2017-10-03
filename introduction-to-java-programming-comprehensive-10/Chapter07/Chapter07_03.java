5743a5654e11555f01f9c7942f7f7cdcabd79fe0
Cosmin Gherghe <cosminlinux@gmail.com>
2017-10-03 14:16:11 +0100

    Create Chapter07_03.java

---
+++ introduction-to-java-programming-comprehensive-10/Chapter07/Chapter07_03.java
@@ -0,0 +1,34 @@
+/*
+ * (Count occurrence of numbers) Write a program that reads the integers between 1
+ *  and 100 and counts the occurrences of each. Assume the input ends with 0. 
+ */ 
+import java.util.*;
+
+public class CountOccurrenceOfNumbers {
+
+	public static void main(String[] args) { 
+		
+		displayOccurrences(askForNumbers());
+	}
+	
+	public static int[] askForNumbers() {
+		Scanner input = new Scanner(System.in);
+		int[] first100numbers = new int[100];
+		System.out.print("Enter the integers between 1 and 100 (input ends with 0): ");
+
+		int test = 0;
+		do{
+			test = input.nextInt();
+			first100numbers[test]++;
+		}while(test != 0);
+		
+		return first100numbers;
+	}
+	
+	public static void displayOccurrences(int[] x) {
+		for (int i = 1; i < x.length; i++) {
+			if(x[i] > 0)
+				System.out.println(i + " occurs " + x[i] + " times");
+		}
+	}
+}
