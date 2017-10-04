2743e58da0e8656925943f575d063803dab49b40
Cosmin Gherghe <cosminlinux@gmail.com>
2017-10-05 00:48:28 +0100

    Update Chapter07_14.java

--- introduction-to-java-programming-comprehensive-10/Chapter07/Chapter07_14.java
+++ introduction-to-java-programming-comprehensive-10/Chapter07/Chapter07_14.java
@@ -27,7 +27,7 @@ public class Computing_gcd {
 	
 	public static int gcd(int... numbers) {
 		
-		int lastDivisors = 1;
+		int lastDivisor = 1;
 		int testResult; 
 				
 		for (int i = 2; ; i++) {
@@ -36,13 +36,13 @@ public class Computing_gcd {
 			for (int j = 0; j < numbers.length; j++) {
 				
 				if(numbers[j] / i == 1)
-					return lastDivisors;	 
+					return lastDivisor;	 
 				else if(numbers[j] % i == 0)
 					testResult--; // i is a divisor (factors) for all numbers if testResult is 0
 			}
 			
 			if(testResult == 0)
-				lastDivisors = i;
+				lastDivisor = i;
 		} 
 	}
 
