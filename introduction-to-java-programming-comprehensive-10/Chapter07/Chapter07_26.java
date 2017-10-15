97a7d3f3da6e56847b85ae144cfde51c64737c87
Cosmin Gherghe <cosminlinux@gmail.com>
2017-10-15 14:40:25 +0100

    Update Chapter07_26.java

--- introduction-to-java-programming-comprehensive-10/Chapter07/Chapter07_26.java
+++ introduction-to-java-programming-comprehensive-10/Chapter07/Chapter07_26.java
@@ -1,3 +1,15 @@
+/** (Strictly identical arrays) The arrays list1 and list2 are strictly identical
+  * if their corresponding elements are equal. Write a method that returns true if
+  * list1 and list2 are strictly identical, using the following header:
+
+  * public static boolean equals(int[] list1, int[] list2)
+
+  * Write a test program that prompts the user to enter two lists of integers and 
+  * displays whether the two are strictly identical. Here are the sample runs. Note that
+  * the first number in the input indicates the number of the elements in the list. This
+  * number is not part of the list
+  */
+
 import java.util.*;
 
 public class HelloWorld {
