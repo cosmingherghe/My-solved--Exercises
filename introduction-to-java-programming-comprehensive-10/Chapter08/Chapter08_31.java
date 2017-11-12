10c78f744e70820452d56b11cb214ddaec31b950
Cosmin Gherghe <cosminlinux@gmail.com>
2017-11-12 15:38:00 +0000

    Update Chapter08_31.java

--- introduction-to-java-programming-comprehensive-10/Chapter08/Chapter08_31.java
+++ introduction-to-java-programming-comprehensive-10/Chapter08/Chapter08_31.java
@@ -18,18 +18,26 @@
 
 import java.util.Scanner;
 
-public class Main {
+public class IntersectingPoint {
 
     public static void main(String[] args) {
 
         double[][] lines = getPoints();
 
-        getIntersectingPoint(lines);
+        double[] abcdef = getIntersectingPoint(lines);
+
+	double x = (abcdef[4] * abcdef[3] - abcdef[1] * abcdef[5] ) / ( abcdef[0] * abcdef[3] - abcdef[1] * abcdef[2] );
+	double y = (abcdef[0] * abcdef[5] - abcdef[4] * abcdef[2] ) / ( abcdef[0] * abcdef[3] - abcdef[1] * abcdef[2] );
+
+	// If the equation has no solutions, the two lines are parallel
+	System.out.println( ( ( abcdef[0] * abcdef[3] - abcdef[1] * abcdef[2] ) == 0 ) ? "null" : "x is " + x + " and y is " + y );
 
 
     }
 
 	public static double[] getIntersectingPoint(double[][] points) {
+
+		double[] abcdef = new double[6];
 
 		double a = y[0][1] - y[1][1];
 		double b = x[0][0] - x[1][0];
@@ -38,11 +46,7 @@ public class Main {
 		double e = ( y[0][1] - y[1][1] ) * x[0][0] - ( x[0][0] - x[1][0] ) * y[0][1];
 		double f = ( y[2][0] - y[3][1] ) * x[2][0] - ( x[2][0] - x[3][0] ) * y[2][0];
 
-		double x = (e*d - b*f) / (a*d - b*c);
-		double y = (a*f - e*c) / (a*d - b*c);
-
-		// If the equation has no solutions, the two lines are parallel
-		System.out.println( ( (a*d - b*c) == 0 ) ? "null" : "x is " + x + " and y is " + y );
+		return abcdef;
 	}
 
 	public static double[][] getPoints() {
