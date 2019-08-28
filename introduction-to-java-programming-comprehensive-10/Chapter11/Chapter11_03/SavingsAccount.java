8a05f213cf539da854cb22b482a9df0196f61153
Cosmin Gherghe <cosminlinux@gmail.com>
2019-08-28 11:35:30 +0100

    Create SavingsAccount.java

---
+++ introduction-to-java-programming-comprehensive-10/Chapter11/Chapter11_03/SavingsAccount.java
@@ -0,0 +1,12 @@
+public class SavingsAccount extends Account {
+  SavingsAccount(){
+  };
+
+  SavingsAccount(int id, double balance){
+    super(id, balance); 
+  }
+
+  public String toString() { 
+    return  "\nClass Name: " + super.getClass().getName() + "\nDate created: " + super.getDateCreated() + "\nAccount number: " + super.getId() + "\nBalance: " + super.getBalance() + "\nAnnual interest rate: " + super.getAnnualInterestRate() + "\nOverdraft: N/A";
+  }
+}
