7edcdb36234980c4d3d0ec731aa6fc2538fd5040
Cosmin Gherghe <cosminlinux@gmail.com>
2019-08-31 17:23:04 +0100

    completed

---
+++ introduction-to-java-programming-comprehensive-10/Chapter11/Chapter11_07/Main.java
@@ -0,0 +1,30 @@
+package Chapter11_07;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		 
+		ArrayList<Integer> intList = new ArrayList<Integer>();
+		intList.add(1);
+		intList.add(2);
+		intList.add(3);
+		intList.add(4);
+		intList.add(5);
+		
+		shuffle(intList);
+		
+		//Display Result
+		for (int i = 0; i < intList.size(); i++) {
+			System.out.println("\nKey(" + i + ") : value: " + intList.get(i));
+		}
+		
+	}
+	
+	//Array List
+	public static void shuffle(ArrayList<Integer> list) {
+		Collections.shuffle(list);
+	}
+
+}
---
+++ introduction-to-java-programming-comprehensive-10/Chapter11/Chapter11_07/README.md
@@ -0,0 +1,5 @@
+# Exercice 11.7
+
+(Shuffle ArrayList) Write the following method that shuffles the elements in an ArrayList of integers.
+
+public static void shuffle(ArrayList<Integer> list) 
