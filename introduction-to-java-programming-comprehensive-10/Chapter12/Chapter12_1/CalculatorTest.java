1ab850a453226a2aeace0d805a6c9b7a8d1132f7
Cosmin Gherghe <cosminlinux@gmail.com>
2020-02-07 15:17:30 +0000

    Update CalculatorTest.java

--- introduction-to-java-programming-comprehensive-10/Chapter12/Chapter12_1/CalculatorTest.java
+++ introduction-to-java-programming-comprehensive-10/Chapter12/Chapter12_1/CalculatorTest.java
@@ -1,9 +1,3 @@
-/* 
-Calculator.java, is a simple commandline calculator. Note that the program terminates if any operand is nonnumeric.
-Write a program with an exception handler that deals with nonnumeric operands;
-Your program should display a message that informs the user of the wrong operand type before exiting. 
-*/
-
 public class CalculatorTest {
     /** Main method */
     public static void main(String[] args) {
