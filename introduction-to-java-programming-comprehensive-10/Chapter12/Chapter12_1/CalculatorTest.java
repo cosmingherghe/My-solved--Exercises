06c92fb9893d814c7aae146ccb8f5959360b78af
Cosmin Gherghe <cosminlinux@gmail.com>
2020-02-07 15:28:22 +0000

    Update CalculatorTest.java

--- introduction-to-java-programming-comprehensive-10/Chapter12/Chapter12_1/CalculatorTest.java
+++ introduction-to-java-programming-comprehensive-10/Chapter12/Chapter12_1/CalculatorTest.java
@@ -3,17 +3,13 @@ public class CalculatorTest {
     public static void main(String[] args) {
     
         //Check number of strings passed 
-        
         if (args.length != 3) {
             System.out.println("Usage: java Calculator operand1 operator operand2");
             System.exit(0);
         } 
          
-        try {
-            //Check Division by a zero 
-            if( Double.valueOf(args[2]) == 0.0 )
-                throw new ArithmeticException("Division by a zero");
-  
+        //Compute calculations
+        try { 
             Calculator calc = new Calculator( Double.valueOf(args[0]), args[1].charAt(0), Double.valueOf(args[2]) );  
             calc.textDisplayResult();
         } catch (Exception e) { 
