b1a3a3bf5c0565c744edaa643c89981fffbb2338
Cosmin Gherghe <cosminlinux@gmail.com>
2020-02-10 18:04:06 +0000

    Create Bin2Dec.java

---
+++ introduction-to-java-programming-comprehensive-10/Chapter12/Chapter12_7/Bin2Dec.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Bin2Dec {
+  public static void main(String[] args) {
+    // Create a Scanner
+    Scanner input = new Scanner(System.in);
+ 
+    // Prompt the user to enter a binary string 
+    System.out.print("Enter a binary string: " );
+    String binaryStr = input.nextLine();
+    input.close();
+
+    try {
+      System.out.println("The decimal value for binary number " + binaryStr + " is " + bin2Dec(binaryStr) );
+   }
+    catch(NumberFormatException ex){
+       System.out.println( ex.getMessage() );
+    } 
+  }
+
+  public static int bin2Dec(String binaryString) 
+    throws NumberFormatException {
+    
+    //check if the string is a binary number
+    for(int i = 0; i < binaryString.length(); i++){
+          //A Binary Number is made up of only 0s and 1s
+          if( !(0 == binaryString.charAt(i) - '0' ||  1 == binaryString.charAt(i) - '0') )
+            throw new NumberFormatException("\nError \n\nA Binary Number is made up of only 0s and 1s.\n\n");
+    }
+
+    int binary = Integer.valueOf(binaryString);
+    int decimal = 0;  
+    int n = 0;  
+    while(true){  
+      if(binary == 0){  
+        break;  
+      } else {  
+          int temp = binary%10;  
+          decimal += temp*Math.pow(2, n);  
+          binary = binary/10;  
+          n++;  
+       }  
+    }  
+    return decimal;  
+  }
+
+}
