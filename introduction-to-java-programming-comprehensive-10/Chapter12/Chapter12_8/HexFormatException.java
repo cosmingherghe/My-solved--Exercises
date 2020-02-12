ed5b29d35ba9241b7baa21412aaf5bfa5df623f8
Cosmin Gherghe <cosminlinux@gmail.com>
2020-02-12 09:26:27 +0000

    Update HexFormatException.java

--- introduction-to-java-programming-comprehensive-10/Chapter12/Chapter12_8/HexFormatException.java
+++ introduction-to-java-programming-comprehensive-10/Chapter12/Chapter12_8/HexFormatException.java
@@ -1,6 +1,11 @@
 public class HexFormatException extends Exception {
   private char hex;
 
+  public HexFormatException() {
+    super("\nError: \nDoes not contain a Hexadecimal symbol.\n\nHexadecimal uses sixteen distinct symbols, most often the symbols 0 to 9 to represent values zero to nine, and A to F to represent values ten to fifteen.\n");
+    this.hex = hex;
+  }
+  
   public HexFormatException(char hex) {
     super("\nError: \nChar '"+ hex +"' is not a Hexadecimal symbol.\n\nHexadecimal uses sixteen distinct symbols, most often the symbols 0 to 9 to represent values zero to nine, and A to F to represent values ten to fifteen.\n");
     this.hex = hex;
