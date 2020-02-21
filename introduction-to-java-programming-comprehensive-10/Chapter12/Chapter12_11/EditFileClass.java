b595eaa77798f32240c9db03e4a86744d1f11df1
Cosmin Gherghe <cosminlinux@gmail.com>
2020-02-21 21:18:59 +0000

    review

--- introduction-to-java-programming-comprehensive-10/Chapter12/Chapter12_11/EditFileClass.java
+++ introduction-to-java-programming-comprehensive-10/Chapter12/Chapter12_11/EditFileClass.java
@@ -17,12 +17,7 @@ public class EditFileClass {
                 this.file = file;
                 this.textInput = textInput;
                 this.tempFile = file + "_temp";
-        }
-
-        // if(!fileData.exists()) {
-        // System.out.println("\n\nFine does not exists.\n\n");
-        // System.exit(0);
-        // }
+        } 
 
         public void deleteAllmatch() throws IOException, FileNotFoundException {
 
@@ -59,16 +54,5 @@ public class EditFileClass {
 
                 File renameFile = new File(this.tempFile);
                 renameFile.renameTo(new File(this.file));
-        }
-        /**
-         * System.out.println("Does it exist? " + ); System.out.println("The file has "
-         * + file.length() + " bytes"); System.out.println("Can it be read? " +
-         * file.canRead()); System.out.println("Can it be written? " + file.canWrite());
-         * System.out.println("Is it a directory? " + file.isDirectory());
-         * System.out.println("Is it a file? " + file.isFile()); System.out.println("Is
-         * it absolute? " + file.isAbsolute()); System.out.println("Is it hidden? " +
-         * file.isHidden()); System.out.println("Absolute path is " +
-         * file.getAbsolutePath()); System.out.println( "Last modified on " + new
-         * java.util.Date(file.lastModified()) );
-         */
+        } 
 }
