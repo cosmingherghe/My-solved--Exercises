4b1a916e284ff4b6488fcf867fa755c564f3f83e
Cosmin Gherghe <hello@cosmingherghe.dev>
2020-12-02 12:56:40 +0000

    Create Recursion1Factorial.java

---
+++ codingbat/Recursion1Factorial.java
@@ -0,0 +1,28 @@
+/*
+Given n of 1 or more, return the factorial of n, which is n * (n-1) * (n-2) ... 1. Compute the result recursively (without loops).
+
+
+factorial(1) → 1
+factorial(2) → 2
+factorial(3) → 6
+*/
+
+
+class Main {  
+  public static void main(String args[]) { 
+
+    System.out.println( factorial( 6 ) ); 
+  } 
+  
+    static public int factorial(int n) {
+  
+      if(n < 2)
+          return n * 1;
+      else {
+        int result = n * factorial(n - 1);
+        n -= 1;
+        return result;
+      }
+        
+    }
+}
