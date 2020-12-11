a4c515e952cb77f221d910ea861859db027dd564
Cosmin Gherghe <hello@cosmingherghe.dev>
2020-12-11 21:40:23 +0000

    Create RemoveEveryVowel.java

---
+++ edabit.com/RemoveEveryVowel.java
@@ -0,0 +1,28 @@
+/*
+Remove Every Vowel from a String
+Create a function that takes a string and returns a new string with all vowels removed.
+
+Examples
+removeVowels("I have never seen a thin person drinking Diet Coke.")
+➞ " hv nvr sn  thn prsn drnkng Dt Ck."
+
+removeVowels("We're gonna build a wall!")
+➞ "W'r gnn bld  wll!"
+
+removeVowels("Happy Thanksgiving to all--even the haters and losers!")
+➞ "Hppy Thnksgvng t ll--vn th htrs nd lsrs!"
+Notes
+"y" is not considered a vowel.
+Expect a valid string for all test cases.
+*/
+
+class RemoveEveryVowel {  
+  public static void main(String args[]) { 
+     System.out.print( removeVowels("Happy Thanksgiving to all--even the haters and losers!") );
+  } 
+  
+  static String removeVowels(String s) {  
+    String regex = "[aeiouAEIOU]";
+    return s.replaceAll(regex, ""); 
+  }
+}
